08f721f6774b1e3ecc0506904bdbea7a21804465
Viper1007 <56436023+Viper1007@users.noreply.github.com>
2020-01-27 19:50:47 +0530

    Add files via upload

---
+++ login form.js
@@ -0,0 +1,19 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+var phoneNumber = document.getElementById("#phone");
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
