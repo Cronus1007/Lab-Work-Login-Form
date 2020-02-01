ca385aed1cc59a6c9d8c64817351da6fae62f106
Viper1007 <56436023+Viper1007@users.noreply.github.com>
2020-02-01 19:01:04 +0530

    Updated

--- login form.js
+++ login form.js
@@ -37,39 +37,53 @@ function checkRadio() {
     }
 }
 
-function submitForm() {
+function formValidation() {
     var studentname = document.getElementById("clg");
     var checkCollegeInput = document.querySelector(".studentinput input");
-    if (studentname.value == "") {
+    if (studentname.value.trim() == "") {
         checkCollegeInput.style.border = "2px inset red"
+    } else {
+        checkCollegeInput.style.border = "2px inset blue"
     }
     var courseName = document.getElementById("course")
     var checkCourseInput = document.querySelectorAll(".studentinput input");
-    if (courseName.value == "") {
+    if (courseName.value.trim() == "") {
         checkCourseInput[1].style.border = "2px inset red"
+    } else {
+        checkCourseInput[1].style.border = "2px inset blue"
     }
     var companyName = document.getElementById("compName");
     var checkCompanyInput = document.querySelectorAll(".professionalinput input");
-    if (companyName.value == "") {
+    if (companyName.value.trim() == "") {
         checkCompanyInput[0].style.border = "2px inset red"
+    } else {
+        checkCompanyInput[0].style.border = "2px inset blue"
     }
     var titleName = document.getElementById("title");
     var checkCompanyInput = document.querySelectorAll(".professionalinput input");
-    if (titleName.value == "") {
+    if (titleName.value.trim() == "") {
         checkCompanyInput[1].style.border = "2px inset red"
+    } else {
+        checkCompanyInput[1].style.border = "2px inset blue"
     }
     var name = document.getElementById("name");
     var checkNameInput = document.querySelectorAll(".overlay-left input")
-    if (name.value == "") {
+    if (name.value.trim() == "") {
         checkNameInput[0].style.border = "2px inset red"
+    } else {
+        checkNameInput[0].style.border = "2px inset white"
     }
     var email = document.getElementById("email");
-    if (email.value == "") {
+    if (email.value.trim() == "") {
         checkNameInput[1].style.border = "2px inset red"
+    } else {
+        checkNameInput[1].style.border = "2px inset white"
     }
     var phone = document.getElementById("phone");
-    if (phone.value == "") {
+    if (phone.value.trim() == "") {
         checkNameInput[2].style.border = "2px inset red"
+    } else {
+        checkNameInput[2].style.border = "2px inset white"
     }
     var gender = document.getElementById("gender");
     var checkSelect = document.querySelectorAll(".overlay-left select")
@@ -77,5 +91,32 @@ function submitForm() {
     if (gender.value == "Gender") {
         checkSelect[0].style.border = "2px inset red"
         console.log(checkSelect)
+    } else {
+        checkSelect[0].style.border = "2px inset white"
+    }
+    submitForm()
+}
+
+function submitForm() {
+    var studentname = document.getElementById("clg");
+    var checkCollegeInput = document.querySelector(".studentinput input");
+    var courseName = document.getElementById("course")
+    var checkCourseInput = document.querySelectorAll(".studentinput input");
+    var companyName = document.getElementById("compName");
+    var checkCompanyInput = document.querySelectorAll(".professionalinput input");
+    var titleName = document.getElementById("title");
+    var checkCompanyInput = document.querySelectorAll(".professionalinput input");
+    var name = document.getElementById("name");
+    var checkNameInput = document.querySelectorAll(".overlay-left input")
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var gender = document.getElementById("gender");
+    var checkSelect = document.querySelectorAll(".overlay-left select")
+    if (studentname.value != "" && courseName != "" && name.value != "" && email.value != "" && phone.value != "" && gender.value != "Gender") {
+        alert("Form Submitted for Student Profile!")
+    } else if (companyName.value != "" && titleName != "" && name.value != "" && email.value != "" && phone.value != "" && gender.value != "Gender") {
+        alert("Form Submitted for Professional Profile!")
+    } else {
+        alert("Vacant Spaces Avlaible while Registering!")
     }
 }
